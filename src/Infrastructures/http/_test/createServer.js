90c266dafde6c07f442b175eba351c084b9f7b04
fadlullah.khan <fadlullah.khan007@gmail.com>
2025-06-11 19:13:09 +0700

    http server test

---
+++ src/Infrastructures/http/_test/createServer.js
@@ -0,0 +1,38 @@
+const pool = require('../../database/postgres/pool');
+const UserTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const container = require('../../container');
+const createServer = require('../createServer');
+
+describe('HTTP server', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await UserTableTestHelper.cleanTable();
+  });
+
+  describe('when POST /users', () => {
+    it('should response 201 and persisted user', async () => {
+      const requestPayload = {
+        username: 'dicoding',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      };
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(reesponse.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedUser).toBeDefined();
+    });
+  });
+});
